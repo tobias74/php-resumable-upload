a2362acd8bef6aa64c618325bbf9da5629254cd2
root <root@AB9EFA7>
2018-05-05 07:19:37 +0000

    work

---
+++ src/ResumableUploadProcessor.php
@@ -0,0 +1,188 @@
+<?php
+namespace PhpResumableUpload;
+
+class ResumableUploadProcessor
+{
+  protected _isComplete=false;
+  
+  public function __construct($config)
+  {
+    $this->config = array_merge(array(
+        'parameterNameResumableIdentifier' => 'resumableIdentifier',
+        'parameterNameResumableFilename' => 'resumableFilename',
+        'parameterNameResumableChunkNumber' => 'resumableChunkNumber',
+        'parameterNameResumableTotalChunks' => 'resumableTotalChunks',
+        'parameterNameResumableTotalSize' => 'resumableTotalSize',
+        'parameterNameFile' => 'file'
+    ), $config);       
+      
+  }
+  
+  
+  protected function getTotalChunks()
+  {
+    return intval($_REQUEST[ $this->config['parameterNameResumableTotalChunks'] ]);
+  }
+
+  protected getChunkNumber()
+  {
+    return intval($_REQUEST[ $this->config['parameterNameResumableChunkNumber'] ]);
+  }
+  
+  protected function getTotalSize()
+  {
+    return intval($_REQUEST[ $this->config['parameterNameResumableTotalSize'] ]);
+  }
+
+  protected function getIdentifier()
+  {
+    return $_REQUEST[ $this->config['parameterNameResumableIdentifier'] ]      
+  }
+
+  public function getOriginalFileName()
+  {
+    return $_REQUEST[ $this->config['parameterNameResumableFilename'] ];  
+  }
+
+
+  protected function getFolderForChunkedUpload()
+  {
+    return sys_get_temp_dir().'/user-'.$this->getLoggedInUserId().'/'.md5($this->getIdentifier());
+  }
+
+
+  protected function getTargetFileName()
+  {
+    return $this->getFolderForChunkedUpload().'/'.md5( $this->getOriginalFileName() );
+  }
+
+  protected function getBaseFileNameForChunks()
+  {
+    return $this->getTargetFileName().'.part';
+  }
+
+  protected function getFileNameForChunkOfCurrentRequest()
+  {
+    return $this->getBaseFileNameForChunks().$this->getChunkNumber());
+  }
+
+  protected function getFileNameForChunkWithNumber($chunkNumber)
+  {
+    return $this->getBaseFileNameForChunks().$chunkNumber;
+  }
+
+  protected function isChunkedUploadComplete()
+  {
+    $totalSizeAllParts = 0;
+    
+    for ($i=1; $i<=intval( $this->getTotalChunks() ); $i++) 
+    {
+      $partName = $this->getFileNameForChunkWithNumber($i);
+      if (file_exists($partName))
+      {
+        $totalSizeAllParts += filesize($partName);
+      }
+    }
+    
+    if ($totalSizeAllParts === $this->getTotalSize() ) 
+    {
+      return true;
+    }
+    else 
+    {
+      return false;  
+    }
+  }
+  
+  protected function createCompleteFileFromChunks()
+  {
+    $fp = fopen($this->getTargetFileName(), 'w'); 
+    for ($i=1; $i <= $this->getTotalChunks(); $i++) 
+    {
+      fwrite($fp, file_get_contents($this->getFileNameForChunkWithNumber($i)));
+      error_log('writing chunk '.$i);
+    }
+    fclose($fp);
+  }
+
+  protected function removeFolderRecursively($dir) 
+  {
+    if (is_dir($dir)) 
+    {
+      $objects = scandir($dir);
+      foreach ($objects as $object) 
+      {
+        if ($object != "." && $object != "..") 
+        {
+          if (filetype($dir . "/" . $object) == "dir") 
+          {
+            $this->removeFolderRecursively($dir . "/" . $object); 
+          } 
+          else 
+          {
+            unlink($dir . "/" . $object);
+          }
+        }
+      }
+      reset($objects);
+      rmdir($dir);
+    }
+  }
+
+
+  public function existsChunk()
+  {
+    if ( $this->getTotalChunks() === $this->getChunkNumber() )
+    {
+       //we do want him to repeat the last chunk to initiate the copy-process.. something might have gon wrong before
+       return false;
+    }
+    else if (file_exists($this->getFileNameForChunkOfCurrentRequest())) 
+    {
+      return true;
+    } 
+    else 
+    {
+      return false
+    }
+      
+  }
+  
+  public function processNextChunk()
+  {
+    $file = $_FILES[ $this->config['parameterNameFile'] ]; 
+
+    $temp_dir = $this->getFolderForChunkedUpload();
+    if (!is_dir($temp_dir)) 
+    {
+      mkdir($temp_dir, 0777, true);
+    }
+    
+    $dest_file = $this->getFileNameForChunkOfCurrentRequest();
+
+    move_uploaded_file($file['tmp_name'], $dest_file);
+
+    if ($this->isChunkedUploadComplete())
+    {
+      $this->createCompleteFileFromChunks();
+      $this->_isComplete = true;
+      return $this;
+    }
+    else 
+    {
+      $this->_isComplete = false;
+      return $this;
+    }
+  }
+  
+  public function isComplete()
+  {
+    return $this->_isComplete;    
+  }
+  
+  public function cleanUpAfterUpload()
+  {
+    $this->removeFolderRecursively( $this->getFolderForChunkedUpload() );
+  }
+    
+}
